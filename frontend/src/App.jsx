113fe64f082ef2c15b818089c78900c028e724fc
JESSICA SHALOMIN SELVAN <jessicashalominselvan@JESSICAs-Laptop.local>
2024-12-12 14:31:18 +0530

    committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import LoginPage from "./component/auth/Login";
-
+import SignupForm from "../src/component/auth/Signup";
+import { Routes, Route } from "react-router-dom";
 function App () {
   return (
     <>
-    <LoginPage />
+    {/* {<LoginPage />} */}
+    {/* <SignupForm /> */}
+    <Routes>
+      <Route path="/"/>
+      <Route path="/signup" element={<SignupForm/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+    </Routes>
     </>
   );
 }
